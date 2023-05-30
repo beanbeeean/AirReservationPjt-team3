f88eb90d828236c81dbd58021d0036c439f90442
beanbeeean <beanbeeean@naver.com>
2023-05-30 17:29:25 +0900

    menu change finished

--- js/signup.js
+++ js/signup.js
@@ -1,134 +1,163 @@
 const addEventsProfile = () => {
+  /* MENU CLICK EVENT START */
+  let signUpMenuBtn = document.querySelector("div.menu_wrap > a.sign_up");
+  signUpMenuBtn.addEventListener("click", function () {
+    console.log("signUpMenuBtn CLICKED!!!!");
 
-    /* MENU CLICK EVENT START */
-    let signUpMenuBtn = document.querySelector('div.menu_wrap > a.sign_up');
-    signUpMenuBtn.addEventListener('click', function () {
-        console.log('signUpMenuBtn CLICKED!!!!');
+    showSelectedView(SIGN_UP_VIEW);
+  });
 
-        showSelectedView(SIGN_UP_VIEW);
-    });
+  let signInMenuBtn = document.querySelector("div.menu_wrap  a.sign_in");
+  signInMenuBtn.addEventListener("click", function () {
+    console.log("signInMenuBtn CLICKED!!!!");
 
-    let signInMenuBtn = document.querySelector('div.menu_wrap  a.sign_in');
-    signInMenuBtn.addEventListener('click', function () {
-        console.log('signInMenuBtn CLICKED!!!!');
+    showSelectedView(SIGN_IN_VIEW);
+  });
 
-        showSelectedView(SIGN_IN_VIEW);
-    });
+  let signOutMenuBtn = document.querySelector("div.menu_wrap > a.sign_out");
+  signOutMenuBtn.addEventListener("click", function () {
+    console.log("signOutMenuBtn CLICKED!!!!");
 
-    let signOutMenuBtn = document.querySelector('div.menu_wrap > a.sign_out');
-    signOutMenuBtn.addEventListener('click', function () {
-        console.log('signOutMenuBtn CLICKED!!!!');
+    signInedMemberId = "";
+    setMenuStatus(SIGN_OUT_STATUS);
+    showSelectedView(SIGN_OUT_VIEW);
+  });
 
-        signInedMemberId = '';
+  let editProfileMenuBtn = document.querySelector(
+    "div.menu_wrap > a.sign_modify"
+  );
+  editProfileMenuBtn.addEventListener("click", function () {
+    console.log("editProfileBtn CLICKED!!!!");
 
-        showSelectedView(SIGN_OUT_VIEW);
-    });
+    let current_id = document.querySelector(
+      "div.sign_modify_wrap div.current_id"
+    );
 
+    current_id.textContent = signInedMemberId;
 
-    let editProfileMenuBtn = document.querySelector('div.menu_wrap > a.sign_modify');
-    editProfileMenuBtn.addEventListener('click', function () {
-        console.log('editProfileBtn CLICKED!!!!');
-        
-        let current_id = document.querySelector('div.sign_modify_wrap div.current_id');
+    showSelectedView(EDIT_PROFILE_VIEW);
+  });
+  /* MENU CLICK EVENT END */
 
-        current_id.textContent = signInedMemberId;
+  /* FUNCTION CLICK EVENT START*/
+  let signUpBtn = document.querySelector("div.sign_up_wrap button.sign_up_btn");
+  signUpBtn.addEventListener("click", function () {
+    console.log("signUpBtn CLICKED!!!");
 
-        showSelectedView(EDIT_PROFILE_VIEW);
-    });
-    /* MENU CLICK EVENT END */
+    let u_id = document.querySelector(
+      'div.sign_up_wrap input[name="u_id"]'
+    ).value;
+    let u_pw = document.querySelector(
+      'div.sign_up_wrap input[name="u_pw"]'
+    ).value;
+    let u_gender = document.querySelector(
+      'div.sign_up_wrap select[name="u_gender"]'
+    ).value;
+    let u_mail = document.querySelector(
+      'div.sign_up_wrap input[name="u_mail"]'
+    ).value;
+    let u_phone = document.querySelector(
+      'div.sign_up_wrap input[name="u_phone"]'
+    ).value;
+    let u_regDate = new Date();
 
+    addMember(u_id, u_pw, u_mail, u_gender, u_phone, u_regDate);
 
-    /* FUNCTION CLICK EVENT START*/
-    let signUpBtn = document.querySelector('div.sign_up_wrap button.sign_up_btn');
-    signUpBtn.addEventListener('click', function () {
-        console.log('signUpBtn CLICKED!!!');
+    alert("SIGN UP SUCCESS!!!");
 
-        let u_id = document.querySelector('div.sign_up_wrap input[name="u_id"]').value;
-        let u_pw = document.querySelector('div.sign_up_wrap input[name="u_pw"]').value;
-        let u_gender = document.querySelector('div.sign_up_wrap select[name="u_gender"]').value;
-        let u_mail = document.querySelector('div.sign_up_wrap input[name="u_mail"]').value;
-        let u_phone = document.querySelector('div.sign_up_wrap input[name="u_phone"]').value;
-        let u_regDate = new Date();
+    document.querySelector('div.sign_up_wrap input[name="u_id"]').value = "";
+    document.querySelector('div.sign_up_wrap input[name="u_pw"]').value = "";
+    document.querySelector('div.sign_up_wrap select[name="u_gender"]').value =
+      "";
+    document.querySelector('div.sign_up_wrap input[name="u_mail"]').value = "";
+    document.querySelector('div.sign_up_wrap input[name="u_phone"]').value = "";
+  });
 
+  let signInBtn = document.querySelector("div.sign_in_wrap button.sign_in_btn");
+  signInBtn.addEventListener("click", function () {
+    console.log("signInBtn CLICKED!!!");
 
-        addMember(u_id, u_pw, u_mail, u_gender, u_phone, u_regDate);
+    let u_id = document.querySelector(
+      'div.sign_in_wrap input[name="u_id"]'
+    ).value;
+    let u_pw = document.querySelector(
+      'div.sign_in_wrap input[name="u_pw"]'
+    ).value;
 
-        alert('SIGN UP SUCCESS!!!');
+    let isMember = searchMember(u_id, u_pw);
 
-        document.querySelector('div.sign_up_wrap input[name="u_id"]').value = '';
-        document.querySelector('div.sign_up_wrap input[name="u_pw"]').value = '';
-        document.querySelector('div.sign_up_wrap select[name="u_gender"]').value = '';
-        document.querySelector('div.sign_up_wrap input[name="u_mail"]').value = '';
-        document.querySelector('div.sign_up_wrap input[name="u_phone"]').value = '';
-    });
+    if (isMember) {
+      alert("SIGN IN SUCCESS!!!");
 
-    
-    let signInBtn = document.querySelector('div.sign_in_wrap button.sign_in_btn');
-    signInBtn.addEventListener('click', function() {
-        console.log('signInBtn CLICKED!!!');
+      signInedMemberId = u_id;
+      // setMenuStatus(SIGN_IN_STATUS);
+    } else {
+      alert("SIGN IN FAIL!!!");
 
-        let u_id = document.querySelector('div.sign_in_wrap input[name="u_id"]').value;
-        let u_pw = document.querySelector('div.sign_in_wrap input[name="u_pw"]').value;
+      signInedMemberId = "";
+    }
+    document.querySelector('div.sign_in_wrap input[name="u_id"]').value = "";
+    document.querySelector('div.sign_in_wrap input[name="u_pw"]').value = "";
+    setMenuStatus(SIGN_IN_STATUS);
+    // showSelectedView("예약페이지")
+  });
 
-        let isMember = searchMember(u_id, u_pw);
+  let editProfileBtn = document.querySelector(
+    "div.sign_modify_wrap button.sign_modify_btn"
+  );
+  editProfileBtn.addEventListener("click", function () {
+    console.log("signUpBtn CLICKED!!!");
 
-        if (isMember) {
+    let u_id = signInedMemberId;
+    let u_pw = document.querySelector(
+      'div.sign_modify_wrap input[name="u_pw"]'
+    ).value;
+    let u_gender = document.querySelector(
+      'div.sign_modify_wrap select[name="u_gender"]'
+    ).value;
+    let u_mail = document.querySelector(
+      'div.sign_modify_wrap input[name="u_mail"]'
+    ).value;
+    let u_phone = document.querySelector(
+      'div.sign_modify_wrap input[name="u_phone"]'
+    ).value;
+    let u_regDate = searchMemberRegDate(u_id);
 
-            alert('SIGN IN SUCCESS!!!');
+    addMember(u_id, u_pw, u_mail, u_gender, u_phone, u_regDate);
 
-            signInedMemberId = u_id;
-            // setMenuStatus(SIGN_IN_STATUS);
+    alert("EDIT SUCCESS!!!");
 
-        } else {
-            
-            alert('SIGN IN FAIL!!!');
+    document.querySelector('div.sign_modify_wrap input[name="u_pw"]').value =
+      "";
+    document.querySelector(
+      'div.sign_modify_wrap select[name="u_gender"]'
+    ).value = "";
+    document.querySelector('div.sign_modify_wrap input[name="u_mail"]').value =
+      "";
+    document.querySelector('div.sign_modify_wrap input[name="u_phone"]').value =
+      "";
+  });
 
-            signInedMemberId ='';
+  let deleteProfileBtn = document.querySelector(
+    "div.sign_modify_wrap > button.delete_user_btn"
+  );
+  deleteProfileBtn.addEventListener("click", function () {
+    let u_pw = document.querySelector(
+      'div.sign_modify_wrap input[name="u_pw"]'
+    ).value;
 
-        }
-        document.querySelector('div.sign_in_wrap input[name="u_id"]').value = '';
-        document.querySelector('div.sign_in_wrap input[name="u_pw"]').value = '';
-    });
+    console.log(u_pw);
+    console.log(typeof u_pw);
+    console.log(searchMemberPW(signInedMemberId));
+    console.log(typeof searchMemberPW(signInedMemberId));
 
-    let editProfileBtn = document.querySelector('div.sign_modify_wrap button.sign_modify_btn');
-    editProfileBtn.addEventListener('click', function () {
-        console.log('signUpBtn CLICKED!!!');
-
-        let u_id = signInedMemberId;
-        let u_pw = document.querySelector('div.sign_modify_wrap input[name="u_pw"]').value;
-        let u_gender = document.querySelector('div.sign_modify_wrap select[name="u_gender"]').value;
-        let u_mail = document.querySelector('div.sign_modify_wrap input[name="u_mail"]').value;
-        let u_phone = document.querySelector('div.sign_modify_wrap input[name="u_phone"]').value;
-        let u_regDate = searchMemberRegDate(u_id);
-
-
-        addMember(u_id, u_pw, u_mail, u_gender, u_phone, u_regDate);
-
-        alert('EDIT SUCCESS!!!');
-       
-        document.querySelector('div.sign_modify_wrap input[name="u_pw"]').value = '';
-        document.querySelector('div.sign_modify_wrap select[name="u_gender"]').value = '';
-        document.querySelector('div.sign_modify_wrap input[name="u_mail"]').value ='';
-       document.querySelector('div.sign_modify_wrap input[name="u_phone"]').value ='';
-    });
-
-    let deleteProfileBtn = document.querySelector('div.sign_modify_wrap > button.delete_user_btn');
-    deleteProfileBtn.addEventListener('click', function() {
-        
-        let u_pw = document.querySelector('div.sign_modify_wrap input[name="u_pw"]').value;
-
-        console.log(u_pw);
-        console.log(typeof u_pw);
-        console.log(searchMemberPW(signInedMemberId));
-        console.log(typeof searchMemberPW(signInedMemberId));
-
-        if (u_pw === searchMemberPW(signInedMemberId)) {
-            let confirmation = confirm('정말 탈퇴하시겠습니까');
-            if (confirmation){
-                deleteProfile(signInedMemberId);
-                alert('삭제되었습니다.');
-            }
-        }
-    })
-    /* FUNCTION CLICK EVENT END*/
-}
+    if (u_pw === searchMemberPW(signInedMemberId)) {
+      let confirmation = confirm("정말 탈퇴하시겠습니까");
+      if (confirmation) {
+        deleteProfile(signInedMemberId);
+        alert("삭제되었습니다.");
+      }
+    }
+  });
+  /* FUNCTION CLICK EVENT END*/
+};
