69446012c38f1183913689b23b8641d224527a4c
RANI <sokoyo2@gmail.com>
2023-05-30 22:14:16 +0900

    js 통합 및 정리

--- js/viewChange.js
+++ js/viewChange.js
@@ -26,40 +26,54 @@ const showSelectedView = (viewNo) => {
 
   switch (viewNo) {
     case SIGN_UP_VIEW:
+
       signUpWrap.style.display = "block";
       signInWrap.style.display = "none";
       editWrap.style.display = "none";
       reservationWrap.style.display = "none";
       reservationListWrap.style.display = "none";
+
       break;
+
     case SIGN_IN_VIEW:
     case SIGN_OUT_VIEW:
+
       signUpWrap.style.display = "none";
       signInWrap.style.display = "block";
       editWrap.style.display = "none";
       reservationWrap.style.display = "none";
       reservationListWrap.style.display = "none";
+
       break;
+
     case EDIT_PROFILE_VIEW:
+
       signUpWrap.style.display = "none";
       signInWrap.style.display = "none";
       editWrap.style.display = "block";
       reservationWrap.style.display = "none";
       reservationListWrap.style.display = "none";
+
       break;
+
     case RESERVATION_VIEW:
+
       signUpWrap.style.display = "none";
       signInWrap.style.display = "none";
       editWrap.style.display = "none";
       reservationWrap.style.display = "block";
       reservationListWrap.style.display = "none";
+
       break;
+
     case RESERVATION_LIST_VIEW:
+
       signUpWrap.style.display = "none";
       signInWrap.style.display = "none";
       editWrap.style.display = "none";
       reservationWrap.style.display = "none";
       reservationListWrap.style.display = "block";
+      
       break;
   }
 };
